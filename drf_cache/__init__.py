628fdc24bd441da1ea81a457aff36075def2c0ce
rainytooo <ohergal@gmail.com>
2019-09-06 16:52:51 +0800

    setup config

---
+++ drf_cache/__init__.py
@@ -0,0 +1,11 @@
+__title__ = 'Django REST framework Caching Tools'
+__version__ = '1.0.0'
+__author__ = 'Vincent Wantchalk'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2011-2019 xsudo.com'
+
+# Version synonym
+VERSION = __version__
+
+# Header encoding (see RFC5987)
+HTTP_HEADER_ENCODING = 'iso-8859-1'
